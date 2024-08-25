a0f3da366e2575a4f87e6c7d13f9d1ba7517d49c
root <root@ip-172-31-27-88.ec2.internal>
2024-08-25 04:42:10 +0000

    update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import boto3
 client = boto3.client('ec2)
 response = client.run_instances(
       ImageId='ami-0680f1a688cd311c',
-      InstanceType='t2micro',
+      InstanceType='x5large',
       KeyName='Public',
       MaxCount=1,
       MinCount=1
